943a999076ec53a12372e6260bcb0edacb011c3b
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2022-12-13 19:24:01 +0300

    tablet updated

--- src/Tablet.java
+++ src/Tablet.java
@@ -2,4 +2,7 @@ public class Tablet extends techEquipments{
     public Tablet(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
         super(brand, model, color, price, expirationYear, isSecondHand);
     }
+    public void play(){
+        System.out.println("it is palying");
+    }
 }
